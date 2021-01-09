24308edd93e9873ee080b0a43d26ed20720bc33d
Lucas Kempe <lucas.kempe@icloud.com>
2021-01-09 16:59:52 -0300

    New get request to get ARS price...

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import './App.css';
 
 class App extends Component {
     render() {
+        const URL = 'https://api.exchangerate-api.com/v4/latest/USD';
+        const getDollar = async () => {
+            const response = await fetch(URL);
+            const data = await response.json();
+            const ARS = data.rates.ARS;
+            console.log(ARS);
+        }
         return (
-            <div className = "App">
-                
+            <div className="App">
+                <h1>Hi</h1>
+                <button onClick = {getDollar}>Get Dollar</button>
             </div>
         );
     }
 }
+
+export default App;
