9869616dfb6ce0aa6bc91eb7a3c7311c546f072d
Lucas Kempe <lucas.kempe@icloud.com>
2021-01-09 17:23:19 -0300

    New get request to get ARS price and parsing into JSON obj...

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './App.css';
 
 class App extends Component {
     render() {
+        var price;
         const URL = 'https://api.exchangerate-api.com/v4/latest/USD';
         const getDollar = async () => {
             const response = await fetch(URL);
             const data = await response.json();
             const ARS = data.rates.ARS;
-            console.log(ARS);
+            price = JSON.parse(ARS);
+            console.log(price);
         }
+        getDollar();
         return (
             <div className="App">
                 <h1>Hi</h1>
-                <button onClick = {getDollar}>Get Dollar</button>
             </div>
         );
     }
