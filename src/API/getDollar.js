fb4dad915d1a9b747da08b1e801024acefc2022e
Lucas Kempe <lucas.kempe@icloud.com>
2021-01-10 09:35:51 -0300

    New event handler for price input...

---
+++ src/API/getDollar.js
@@ -0,0 +1,13 @@
+//import axios from "axios";
+
+var ARS;
+const URL = 'https://api.exchangerate-api.com/v4/latest/USD';
+
+export const getDollar = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    const price = data.rates.ARS;
+    ARS = JSON.parse(price);
+    console.log(price);
+    return ARS;
+} 
--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { getDollar } from './API/getDollar';
 
-class App extends Component {
-    render() {
-        var price;
-        const URL = 'https://api.exchangerate-api.com/v4/latest/USD';
-        const getDollar = async () => {
-            const response = await fetch(URL);
-            const data = await response.json();
-            const ARS = data.rates.ARS;
-            price = JSON.parse(ARS);
-            console.log(price);
+const App = () => {
+
+    const price = useState('');
+
+    const calculate = async (e) => {
+        if(e.key === 'Enter') {
+            const priceIn = await getDollar()
+            console.log(priceIn);
         }
-        getDollar();
-        return (
-            <div className="App">
-                <h1>Hi</h1>
-            </div>
-        );
     }
+
+    return (
+        <div className="App">
+            <h1>Dollary</h1>
+            <input
+                type="text"
+                className="inputPrice"
+                placeholder="Ingrese el precio del producto..."
+                onChange={(e) => price(e.target.value)}
+                onKeyPress={calculate}
+            />
+        </div>
+    );
 }
 
 export default App;
