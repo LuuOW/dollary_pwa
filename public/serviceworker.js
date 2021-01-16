3d8580631037e0272ac59dd87e79df11fc72e23b
Lucas Kempe <lucas.kempe@icloud.com>
2021-01-16 10:21:09 -0300

    Backend finished, service_worker up and running...

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,46 +1,78 @@
 const CACHE_NAME = "version-1"
-const urlsToCache = [ 'index.html', 'offline.html '];
+const urlsToCache = [
+    'index.html',
+    'offline.html'
+];
 
 const self = this;
 
 // Install SW
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function (event) {
+    // Perform install steps
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then((cache) => {
-            console.log('Opened cache');
-
-            return cache.addAll(urlsToCache);
-        })
-    )
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
 // Listen for requests
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request)
-        .then(() => {
-            return fetch(event.request)
-            .catch(() => caches.match('offline.html'))
-        })
-    )
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+            )
+    );
 });
 
 // Activate the SW
 
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [];
-    cacheWhitelist.push(CACHE_NAME);
-
-    event.waitUntil(
-        caches.keys().then((cachesNames) => Promise.all(
-            cachesNames.map((cacheName) => {
-                if(!cacheWhitelist.includes(cacheName)) {
-                    return caches.delete(cacheName);
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
                 }
+
+                // IMPORTANT: Clone the request. A request is a stream and
+                // can only be consumed once. Since we are consuming this
+                // once by cache and once by the browser for fetch, we need
+                // to clone the response.
+                var fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    function (response) {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // IMPORTANT: Clone the response. A response is a stream
+                        // and because we want the browser to consume the response
+                        // as well as the cache consuming the response, we need
+                        // to clone it so we have two streams.
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function (cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
             })
-        ))
-    )
+    );
 });
