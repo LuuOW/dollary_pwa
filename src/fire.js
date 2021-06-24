f784e7ee1430b8d70a52c90e62b9ff42cc4c8761
NonOfUrBusiness <lucas.kempe@icloud.com>
2021-06-23 21:11:57 -0300

    script type defined...

--- src/fire.js
+++ src/fire.js
@@ -2,7 +2,6 @@ var deferredPrompt;
 
 function openCreatePostModel() {
     if (deferredPrompt) {
-        //console.log("4");
         console.log("entro para el baner");
         deferredPrompt.prompt();
         deferredPrompt.userChoice.then(function (choiceResult) {
@@ -21,18 +20,7 @@ function openCreatePostModel() {
     'use strict';
     var app = {
     };
-    // TODO add service worker code here
-    if ('serviceWorker' in navigator) {
-        //console.log("app aqui si regsw");
-        navigator.serviceWorker
-            //.register('/info/pwa/service-worker.js')
-            .register('/info/pwa/serviceWorker-EDET.js')
-            .then(function () { console.log('Service Worker Registered'); });
-    }
-    //console.log("2");
     window.addEventListener('beforeinstallprompt', function (event) {
-        //console.log("beforeinstallprompt fired");
-        //event.preventDefault();
         deferredPrompt = event;
         openCreatePostModel();
         return false;
